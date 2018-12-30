c078f61c189bdddb91022b352d238b862471f361
Vita Smid <vita@quantlane.com>
2018-12-30 12:08:10 +0100

    I want `ag` to search in hidden files

--- bash.sh
+++ bash.sh
@@ -103,7 +103,7 @@ alias ga='git add'
 alias gb='git checkout'
 alias gp='git push'
 alias gpo='git push origin'
-alias gg="ag --pager='less -RFX' --case-sensitive"
+alias gg="ag --pager='less -RFX' --case-sensitive --hidden"
 alias upg='git fetch --prune && git fetch --tags' # update git
 alias grv='git remote -v'
 alias gpu='git pull'
